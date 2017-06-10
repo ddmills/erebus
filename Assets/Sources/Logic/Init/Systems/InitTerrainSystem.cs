47505bcee3118214b3a39c57f9eec565112ff850
ddmills <mills.dalton@gmail.com>
2017-06-10 18:57:12 -0500

    use plane for terrain

--- Assets/Sources/Logic/Init/Systems/InitTerrainSystem.cs
+++ Assets/Sources/Logic/Init/Systems/InitTerrainSystem.cs
@@ -1,4 +1,5 @@
 using Entitas;
+using UnityEngine;
 
 public sealed class InitTerrainSystem : IInitializeSystem {
   private readonly GameContext context;
@@ -10,11 +11,12 @@ public sealed class InitTerrainSystem : IInitializeSystem {
   public void Initialize() {
     var mapSize = context.globals.value.mapSize;
     var tileSize = context.globals.value.tileSize;
+    var offset = (float) tileSize / 2;
     for (var i = 0; i < mapSize; i++) {
       for (var j = 0; j < mapSize; j++) {
         var entity = context.CreateEntity();
-        entity.AddPosition(i * tileSize, 0, j * tileSize);
-        entity.AddAsset("Prefabs/Cube");
+        entity.AddPosition(i * tileSize + offset, 0, j * tileSize + offset);
+        entity.AddAsset("Prefabs/Plane");
       }
     }
   }
