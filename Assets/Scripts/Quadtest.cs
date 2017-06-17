6eb8358e7ba18644b6a371373570194773041c80
Joseph Parker <joseph.parker.1@gmail.com>
2017-06-17 08:28:13 -0400

    QuadTerrain, insertion/deletion mouse demo

--- Assets/Scripts/Quadtest.cs
+++ Assets/Scripts/Quadtest.cs
@@ -1,61 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TestObject : IQuadTreeObject
-{
-    private Vector3 m_vPosition;
-    public TestObject(Vector3 position)
-    {
-        m_vPosition = position;
-    }
-    public Vector2 GetPosition()
-    {
-        //Ignore the Y position, Quad-trees operate on a 2D plane.
-        return new Vector2(m_vPosition.x, m_vPosition.z);
-    }
-}
-
 public class Quadtest : MonoBehaviour
 {
     public GameObject terrainCube;
     private GameObject holder;
-    public QuadTree<TestObject> quad;
+    public TerrainTree quad;
     float seed = 0;
 
     public float Height(float x, float y)
     {
-        var perlinX = seed + 1000 + x / 15f;
-        var perlinY = seed + 1000 + y / 15f;
+        var perlinX = seed + 1000f + x / 15f;
+        var perlinY = seed + 1000f + y / 15f;
         return Mathf.PerlinNoise(perlinX, perlinY);
     }
 
     void Start()
     {
         holder = new GameObject("holder");
-        quad = new QuadTree<TestObject>(1, new Rect(0, 0, 128 , 128));
-        for (int y = 0; y < 128; y++)
+        quad = new TerrainTree(new Rect(0, 0, 120 , 120));
+        for (int y = 0; y < 120; y++)
         {
-            for (int x = 0; x < 128; x++)
+            for (int x = 0; x < 120; x++)
             {   
                 if (Height(x,y) > 0.5)
                 {
-                    Vector3 v = new Vector3(x, 0, y);
-                    TestObject newObject = new TestObject(v);
-                    quad.Insert(newObject);
-                    //GameObject o = Instantiate(terrainCube);
-                    //o.transform.position = v+new Vector3(0.5f,0,0.5f);
-                    //o.transform.parent = holder.transform;
+                    Vector2 v = new Vector2(x, y);
+                    quad.Insert(v);
                 }
-
             }
         }
-        quad.PlaceObject(terrainCube, holder);
+        
     }
 
-
-    void Update()
+    private void Update()
     {
+        quad.Visualize(terrainCube, holder);
+    }
 
+    private void OnMouseOver()
+    {
+        RaycastHit hit = new RaycastHit();
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector2 v = new Vector2(hit.point.x, hit.point.z);
+            if (Input.GetMouseButton(0)) {
+                quad.Insert(v);
+            } else if (Input.GetMouseButton(1)) {
+                quad.Remove(v);
+            }
+
+        }
     }
 
     void OnDrawGizmos()
