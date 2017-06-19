058ffead5591fe9d181014c486544b7ab3318ec9
ddmills <mills.dalton@gmail.com>
2017-06-18 20:20:12 -0500

    lint

--- Assets/Scripts/Quadtest.cs
+++ Assets/Scripts/Quadtest.cs
@@ -1,75 +1,73 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Quadtest : MonoBehaviour
-{
-    public GameObject terrainCube;
-    private GameObject holder;
-    public TerrainTree quad;
-    float seed = 0;
-    private Vector3 mousepos;
+public class Quadtest : MonoBehaviour {
+  public GameObject terrainCube;
+  private GameObject holder;
+  public TerrainTree quad;
+  float seed = 0;
+  private Vector3 mousepos;
 
-    public float Height(float x, float y)
+  public float Height(float x, float y)
+  {
+    var perlinX = seed + 1000f + x / 15f;
+    var perlinY = seed + 1000f + y / 15f;
+    return Mathf.PerlinNoise(perlinX, perlinY);
+  }
+
+  void Start()
+  {
+    holder = new GameObject("holder");
+    quad = new TerrainTree(new Rect(0, 0, 120 , 120));
+    for (int y = 0; y < 120; y++)
     {
-        var perlinX = seed + 1000f + x / 15f;
-        var perlinY = seed + 1000f + y / 15f;
-        return Mathf.PerlinNoise(perlinX, perlinY);
-    }
-
-    void Start()
-    {
-        holder = new GameObject("holder");
-        quad = new TerrainTree(new Rect(0, 0, 120 , 120));
-        for (int y = 0; y < 120; y++)
+      for (int x = 0; x < 120; x++)
+      {
+        if (Height(x,y) > 0.5)
         {
-            for (int x = 0; x < 120; x++)
-            {   
-                if (Height(x,y) > 0.5)
-                {
-                    Vector2 v = new Vector2(x, y);
-                    quad.Insert(v);
-                }
-            }
+          Vector2 v = new Vector2(x, y);
+          quad.Insert(v);
         }
-        
+      }
     }
 
-    private void Update()
+  }
+
+  private void Update()
+  {
+    quad.Visualize(terrainCube, holder);
+  }
+
+  private void OnMouseOver()
+  {
+    RaycastHit hit = new RaycastHit();
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    if (Physics.Raycast(ray, out hit))
     {
-        quad.Visualize(terrainCube, holder);
-    }
+      mousepos = hit.point;
+      Vector2 v = new Vector2(hit.point.x, hit.point.z);
+      if (Input.GetMouseButton(0)) {
+        quad.Insert(v);
+      } else if (Input.GetMouseButton(1)) {
+        quad.Remove(v);
+      }
 
-    private void OnMouseOver()
+    }
+  }
+
+  void OnDrawGizmos()
+  {
+    if (mousepos != null)
     {
-        RaycastHit hit = new RaycastHit();
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit))
-        {
-            mousepos = hit.point;
-            Vector2 v = new Vector2(hit.point.x, hit.point.z);
-            if (Input.GetMouseButton(0)) {
-                quad.Insert(v);
-            } else if (Input.GetMouseButton(1)) {
-                quad.Remove(v);
-            }
-
-        }
+      Gizmos.color = new Color(1, 0, 1);
+      Gizmos.DrawRay(mousepos,new Vector3(1,0,0));
+      Gizmos.DrawRay(mousepos, new Vector3(-1, 0, 0));
+      Gizmos.DrawRay(mousepos, new Vector3(0, 0, 1));
+      Gizmos.DrawRay(mousepos, new Vector3(0, 0, -1));
+      Gizmos.color = new Color(1, 1, 1);
     }
-
-    void OnDrawGizmos()
+    if (quad != null)
     {
-        if (mousepos != null)
-        {
-            Gizmos.color = new Color(1, 0, 1);
-            Gizmos.DrawRay(mousepos,new Vector3(1,0,0));
-            Gizmos.DrawRay(mousepos, new Vector3(-1, 0, 0));
-            Gizmos.DrawRay(mousepos, new Vector3(0, 0, 1));
-            Gizmos.DrawRay(mousepos, new Vector3(0, 0, -1));
-            Gizmos.color = new Color(1, 1, 1);
-        }
-        if (quad != null)
-        {
-            quad.DrawDebug();
-        }
+      quad.DrawDebug();
     }
+  }
 }
--- Assets/Scripts/TerrainTree.cs
+++ Assets/Scripts/TerrainTree.cs
@@ -1,146 +1,125 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 
+public class TerrainTree {
+  public TerrainTree[] cells;
+  public bool solid = false;
+  public Rect bounds;
+  public GameObject visual;
 
-public class TerrainTree
-{
-    public TerrainTree[] cells;
-    public bool solid = false;
-    public Rect bounds;
-    public GameObject visual;
+  public TerrainTree(Rect bounds, bool root) {
+    cells = new TerrainTree[4];
+    this.bounds = bounds;
+  }
 
-    public TerrainTree(Rect _bounds, bool root)
-    {
-        cells = new TerrainTree[4];
-        bounds = _bounds;
+  public TerrainTree(Rect bounds) {
+    bounds.width = ContainingPowerOf2(bounds.width);
+    bounds.height = ContainingPowerOf2(bounds.height);
+    cells = new TerrainTree[4];
+    this.bounds = bounds;
+  }
+
+  public bool Filled() {
+    if (cells[0] == null) {
+      return solid;
     }
-    public TerrainTree(Rect _bounds)
-    {
-        _bounds.width = ContainingPowerOf2(_bounds.width);
-        _bounds.height = ContainingPowerOf2(_bounds.height);
-        cells = new TerrainTree[4];
-        bounds = _bounds;
-    }
+    return (cells[0].Filled() && cells[1].Filled() && cells[2].Filled() && cells[3].Filled());
+  }
 
-    public bool Filled()
-    {
-        if (cells[0] == null)
-        {
-            return solid;
-        } else
-        {
-            return (cells[0].Filled() && cells[1].Filled() && cells[2].Filled() && cells[3].Filled());
-        }
+  public void Insert(Vector2 pos) {
+    if (bounds.width < 1.1) {
+      solid = true;
+      return;
     }
 
-    public void Insert(Vector2 pos)
-    {   
-        if (bounds.width < 1.1)
-        {
-            solid = true;
-            return;
-        }
-        if (cells[0] == null)
-        {
-            float subWidth = (bounds.width / 2f);
-            float subHeight = (bounds.height / 2f);
-            float x = bounds.x;
-            float y = bounds.y;
-            cells[0] = new TerrainTree(new Rect(x + subWidth, y, subWidth, subHeight), false);
-            cells[1] = new TerrainTree(new Rect(x, y, subWidth, subHeight), false);
-            cells[2] = new TerrainTree(new Rect(x, y + subHeight, subWidth, subHeight), false);
-            cells[3] = new TerrainTree(new Rect(x + subWidth, y + subHeight, subWidth, subHeight), false);
-        }
-        int iCell = GetCellToInsertObject(pos);
-        if (iCell > -1)
-        {
-            cells[iCell].Insert(pos);
-        }
+    if (cells[0] == null) {
+      var subWidth = (bounds.width / 2f);
+      var subHeight = (bounds.height / 2f);
+      var x = bounds.x;
+      var y = bounds.y;
+
+      cells[0] = new TerrainTree(new Rect(x + subWidth, y, subWidth, subHeight), false);
+      cells[1] = new TerrainTree(new Rect(x, y, subWidth, subHeight), false);
+      cells[2] = new TerrainTree(new Rect(x, y + subHeight, subWidth, subHeight), false);
+      cells[3] = new TerrainTree(new Rect(x + subWidth, y + subHeight, subWidth, subHeight), false);
     }
 
-    public void Remove(Vector2 pos)
-    {
-        if (bounds.Contains(pos))
-        {
-            if (cells[0] != null)
-            {
-                for (int i = 0; i < 4; i++)
-                {
-                    cells[i].Remove(pos);
-                }
-            } else {
-                solid = false;
-            }
+    var iCell = GetCellToInsertObject(pos);
+    if (iCell > -1) {
+      cells[iCell].Insert(pos);
+    }
+  }
+
+  public void Remove(Vector2 pos) {
+    if (bounds.Contains(pos)) {
+      if (cells[0] != null) {
+        for (var i = 0; i < 4; i++) {
+          cells[i].Remove(pos);
         }
+      } else {
+        solid = false;
+      }
+    }
+  }
+
+  private int GetCellToInsertObject(Vector2 location) {
+    for (var i = 0; i < 4; i++) {
+      if (cells[i].bounds.Contains(location)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public float ContainingPowerOf2(float n) {
+    var res = 2f;
+    while (res < n) {
+      res = Mathf.Pow(res, 2);
+    }
+    return res;
+  }
+
+  public void DrawDebug() {
+    if (Filled()) {
+      Gizmos.DrawLine(new Vector3(bounds.x, 0, bounds.y), new Vector3(bounds.x, 0, bounds.y + bounds.height));
+      Gizmos.DrawLine(new Vector3(bounds.x, 0, bounds.y), new Vector3(bounds.x + bounds.width, 0, bounds.y));
+      Gizmos.DrawLine(new Vector3(bounds.x + bounds.width, 0, bounds.y), new Vector3(bounds.x + bounds.width, 0, bounds.y + bounds.height));
+      Gizmos.DrawLine(new Vector3(bounds.x, 0, bounds.y + bounds.height), new Vector3(bounds.x + bounds.width, 0, bounds.y + bounds.height));
+      return;
     }
 
-    private int GetCellToInsertObject(Vector2 location)
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            if (cells[i].bounds.Contains(location))
-                return i;
+    if (cells[0] != null) {
+      for (var i = 0; i < cells.Length; i++) {
+        if (cells[i] != null) {
+          cells[i].DrawDebug();
         }
-        return -1;
+      }
     }
+  }
 
-    public float ContainingPowerOf2(float n)
-    {
-        float res = 2;
-        while (res < n)
-            res = Mathf.Pow(res, 2);
-        return res;
-    }
+  public void Visualize(GameObject template, GameObject holder) {
+    if (Filled()) {
+      if (visual == null) {
+        var loc = new Vector3(bounds.x + bounds.width / 2f, 0f, bounds.y + bounds.height / 2f);
+        var scl = new Vector3(bounds.width, 1, bounds.height);
+        var instance = GameObject.Instantiate(template);
+        instance.transform.position = loc;
+        instance.transform.localScale = scl;
+        instance.transform.parent = holder.transform;
+        visual = instance;
+      }
+    } else {
+      if (visual != null) {
+        GameObject.Destroy(visual);
+        visual = null;
+      }
 
-    public void DrawDebug()
-    {
-        if (this.Filled())
-        {
-            Gizmos.DrawLine(new Vector3(bounds.x, 0, bounds.y), new Vector3(bounds.x, 0, bounds.y + bounds.height));
-            Gizmos.DrawLine(new Vector3(bounds.x, 0, bounds.y), new Vector3(bounds.x + bounds.width, 0, bounds.y));
-            Gizmos.DrawLine(new Vector3(bounds.x + bounds.width, 0, bounds.y), new Vector3(bounds.x + bounds.width, 0, bounds.y + bounds.height));
-            Gizmos.DrawLine(new Vector3(bounds.x, 0, bounds.y + bounds.height), new Vector3(bounds.x + bounds.width, 0, bounds.y + bounds.height));
-            return;
+      if (cells[0] != null) {
+        for (var i = 0; i < cells.Length; i++) {
+          if (cells[i] != null) {
+            cells[i].Visualize(template, holder);
+          }
         }
-        if (cells[0] != null)
-        {
-            for (int i = 0; i < cells.Length; i++)
-            {
-                if (cells[i] != null)
-                {
-                    cells[i].DrawDebug();
-                }
-            }
-        }
+      }
     }
-
-    public void Visualize(GameObject g, GameObject holder)
-    {
-        if (Filled()) {
-            if (visual == null) {
-                Vector3 loc = new Vector3(bounds.x + bounds.width / 2f, 0f, bounds.y + bounds.height / 2f);
-                Vector3 scl = new Vector3(bounds.width, 1, bounds.height);
-                GameObject o = GameObject.Instantiate(g);
-                o.transform.position = loc;
-                o.transform.localScale = scl;
-                o.transform.parent = holder.transform;
-                visual = o;
-            }
-        } else {
-            if (visual != null) {
-                GameObject.Destroy(visual);
-                visual = null;
-            }
-            if (cells[0] != null)
-            {
-                for (int i = 0; i < cells.Length; i++)
-                {
-                    if (cells[i] != null)
-                    {
-                        cells[i].Visualize(g, holder);
-                    }
-                }
-            }
-        }
-    }
+  }
 }
