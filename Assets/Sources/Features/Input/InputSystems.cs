d19ef1ed1ac5aed9ddd58eb37377e0a49c24f4a2
ddmills <mills.dalton@gmail.com>
2017-07-04 00:09:17 -0500

    add jumpy camera panning

--- Assets/Sources/Features/Input/InputSystems.cs
+++ Assets/Sources/Features/Input/InputSystems.cs
@@ -3,5 +3,6 @@ using Entitas;
 public sealed class InputSystems : Feature {
   public InputSystems(Contexts contexts) : base("Input Systems") {
     Add(new MouseInputSystem(contexts));
+    Add(new CameraControlSystem(contexts));
   }
 }
---
+++ Assets/Sources/Features/Input/Systems/CameraControlSystem.cs
@@ -0,0 +1,53 @@
+using Entitas;
+using UnityEngine;
+using System.Collections.Generic;
+
+public sealed class CameraControlSystem : ReactiveSystem<InputEntity> {
+  private readonly InputContext context;
+  private float previousX;
+  private float previousY;
+  private Camera camera;
+  private Transform cameraHandle;
+  private GameEntity timeEntity;
+  private float panSpeed;
+
+  public CameraControlSystem(Contexts contexts) : base(contexts.input) {
+    context = contexts.input;
+    camera = Camera.main;
+    cameraHandle = camera.transform.parent;
+    timeEntity = contexts.game.timeEntity;
+    panSpeed = 4;
+    previousX = 0;
+    previousY = 0;
+  }
+
+  protected override ICollector<InputEntity> GetTrigger(IContext<InputEntity> context) {
+    return context.CreateCollector(
+      InputMatcher.AllOf(
+        InputMatcher.MousePosition,
+        InputMatcher.MiddleMouse
+      )
+    );
+  }
+
+  protected override bool Filter(InputEntity entity) {
+    return entity.isMiddleMouse && entity.hasMousePosition;
+  }
+
+  protected override void Execute(List<InputEntity> entities) {
+    float mouseOffsetX = context.middleMouseEntity.mousePosition.x - previousX;
+    float mouseOffsetY = context.middleMouseEntity.mousePosition.y - previousY;
+
+    Vector3 direction = new Vector3(cameraHandle.forward.x, 0, cameraHandle.forward.z);
+    direction.Normalize();
+
+    Vector3 forward = direction * timeEntity.time.delta * panSpeed * mouseOffsetY;
+    Vector3 right = Vector3.right * timeEntity.time.delta * panSpeed * mouseOffsetX;
+
+    cameraHandle.Translate(right, Space.Self);
+    cameraHandle.Translate(forward, Space.World);
+
+    previousX = context.middleMouseEntity.mousePosition.x;
+    previousY = context.middleMouseEntity.mousePosition.y;
+  }
+}
